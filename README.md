d50bad9c0b6a7d6c0a3c588cf4dc233dd22a27f5
caetanojs <caetanoj.s@gmail.com>
2021-11-23 20:16:03 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Olympics-Performance-Analysis
+
+### How to install:
+
+First we need to install Python:
+`https://www.python.org/downloads/`
+
+And them we install flask:
+`https://flask.palletsprojects.com/en/2.0.x/installation/#install-flask`
+
+after that we first run `venv/bin/activate` and them `flask run`
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/graphs')
+def graphs():
+    text = request.args.get('jsdata')
+
+    if text == 'sss':
+        suggestions_list = ['teste', '2']
+    else:
+        suggestions_list = ['caetano', 'PDE']
+    
+    return render_template('graphs.html', suggestions=suggestions_list)
